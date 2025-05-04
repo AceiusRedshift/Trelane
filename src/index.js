789f527f5b3bd2ec2bda3047962514c313b374fc
Aceius E. <io@acei.us>
2025-05-03 22:39:29 -0700

    That's enough for today

--- src/index.js
+++ src/index.js
@@ -3,8 +3,30 @@ import "../node_modules/@mielo-ui/mielo/css/mielo.css";
 
 const root = document.getElementById("app");
 
+function button(text, onclick, css) {
+    return m(
+        "button",
+        {
+            className: "mie button pilled " + css,
+            onclick: onclick
+        },
+        text
+    );
+}
+
 let Splash = {
-    view: () => m("a", {href: "#!/app"}, "Enter!")
+    view: () => [
+        m("div", {className: "mie header large center"}, [
+            m("div", {className: "heading"}, [
+                m("div", {className: "title"}, "Trelane"),
+                m("div", {className: "subtitle"}, "A semi-competent SRS"),
+            ]),
+        ]),
+        m("div", {className: "mie buttons"}, [
+            button("New Deck", () => m.route.set("/app")),
+            button("Load Deck", () => m.route.set("/app"), "accent")
+        ])
+    ]
 }
 
 let App = {

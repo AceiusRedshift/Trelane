c218a340bf22a6e90ab508e81dbebd4303900a62
Aceius E. <io@acei.us>
2025-05-03 21:41:17 -0700

    Fixed hotreload in firefox

--- src/hotreload.js
+++ src/hotreload.js
@@ -1,1 +1,10 @@
-new EventSource("/esbuild").addEventListener("change", () => location.reload());
+function setupHotReload() {
+    try {
+        new EventSource("/esbuild").addEventListener("change", () => location.reload());
+        console.info("Hot reload initialized.");
+    } catch (e) {
+        setupHotReload();
+    }
+}
+
+document.addEventListener('DOMContentLoaded', setupHotReload);

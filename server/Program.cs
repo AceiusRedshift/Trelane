77e0022e202bbb6b98b2d042882a33732383aaea
Aceius E. <io@acei.us>
2025-05-08 18:19:12 -0700

    Sort of realizing i have no clue what im doing

--- server/Program.cs
+++ server/Program.cs
@@ -1,13 +1,6 @@
-using Microsoft.EntityFrameworkCore;
-using Trelane.Server.Areas.Identity.Data.Trelane;
-using Trelane.Server.Data.Trelane;
-
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("ServerContextConnection") ?? throw new InvalidOperationException("Connection string 'ServerContextConnection' not found.");
-builder.Services.AddDbContext<ServerContext>(options => options.UseSqlServer(connectionString));
-builder.Services.AddDefaultIdentity<ServerUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ServerContext>();
-
 var app = builder.Build();
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();

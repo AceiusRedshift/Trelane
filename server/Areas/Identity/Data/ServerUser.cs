e3c0463c725731fdbb7892046a53c5ac22c7eb22
Aceius E. <io@acei.us>
2025-05-08 18:00:41 -0700

    Scaffhold identity functions

---
+++ server/Areas/Identity/Data/ServerUser.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Trelane.Server.Areas.Identity.Data.Trelane;
+
+// Add profile data for application users by adding properties to the ServerUser class
+public class ServerUser : IdentityUser
+{
+}
+
--- server/Program.cs
+++ server/Program.cs
@@ -1,6 +1,13 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.EntityFrameworkCore;
+using Trelane.Server.Areas.Identity.Data.Trelane;
+using Trelane.Server.Data.Trelane;
 
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ServerContextConnection") ?? throw new InvalidOperationException("Connection string 'ServerContextConnection' not found.");
+builder.Services.AddDbContext<ServerContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddDefaultIdentity<ServerUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ServerContext>();
+
+var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();

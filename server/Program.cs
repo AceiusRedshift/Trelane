bafd6776151945c1b9338731ee0bb5ac75e02121
Aceius E. <io@acei.us>
2025-05-08 18:38:14 -0700

    Session working?

--- server/Program.cs
+++ server/Program.cs
@@ -1,6 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromDays(2);
+    options.Cookie.IsEssential = true;
+});
 
+var app = builder.Build();
+app.UseSession();
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/-", Dash);
+
+Task Dash(HttpContext context)
+{
+    int timesVisited = context.Session.GetInt32("v") ?? 0;
+    timesVisited++;
+    context.Session.SetInt32("v", timesVisited);
+    return context.Response.WriteAsync($"Visited: {timesVisited} | {DateTime.Now}");
+}
 
 app.Run();
